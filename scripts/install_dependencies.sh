a31fa5faca8425a889ccdc49d6710874afcd3028
epm0dev <epmancini@live.com>
2021-01-18 18:26:10 -0500

    Start Docker service install_dependencies.sh script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,9 @@
 
 # Install docker
 yum -y install docker
+service docker start
+usermod -a -G docker ec2-user
+chkconfig docker on
 
 # Install docker-compose
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
